d2982f50e1c94ddbad9c7684c63b24823626c3e8
Pete1701 <pete.petrongonas@pirron.one>
2020-01-31 21:27:05 +0100

    Finish prototype

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,25 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+function CuboidMaker (attributes){
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;
+}
+
+CuboidMaker.prototype.volume = function(){
+    return length * width * height;
+}
+
+CuboidMaker.prototype.surfaceArea = function(){
+  return 2 * (length * width + length * height + width * height);
+}
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
